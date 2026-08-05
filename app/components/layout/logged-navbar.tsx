23bec6d298c1143d7f13cd35fd00a8d4adabb1e3
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2026-08-05 21:45:02 +0200

    repair navbar

--- app/components/layout/logged-navbar.tsx
+++ app/components/layout/logged-navbar.tsx
@@ -17,7 +17,7 @@ export function Navbar() {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 flex h-20 items-center justify-between border-b border-black/5 bg-[#004a8f] px-4 shadow-[0_4px_12px_rgba(0,0,0,0.2)]">
-      <div className="relative">
+      <div className="relative ml-auto mr-2 md:mr-6">
         <button
           type="button"
           onClick={() => setIsOpen(!isOpen)}
